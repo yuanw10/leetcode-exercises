d64d374c8061a475179d54fb9b722abc144d4b8b
阿苗仔是一只喵 <43899201+yuanw10@users.noreply.github.com>
2023-09-29 21:57:07 -0300

    Time: 39 ms (92.59%), Space: 13 MB (98.15%) - LeetHub

--- 2785-semi-ordered-permutation/2785-semi-ordered-permutation.py
+++ 2785-semi-ordered-permutation/2785-semi-ordered-permutation.py
@@ -6,9 +6,13 @@ class Solution(object):
         """
         count = 0
         min_index = nums.index(1)
-        count += min_index  # num of swaps for 1
-        nums.remove(1)
-        nums.insert(0,1)
         max_index = nums.index(len(nums))
-        count += len(nums)-1-max_index   # num of swaps for n
+        count += min_index  # num of swaps for 1
+
+        # num of swaps for n
+        if max_index < min_index:
+            count += len(nums)-2-max_index
+        else:
+            count += len(nums)-1-max_index
+
         return count
